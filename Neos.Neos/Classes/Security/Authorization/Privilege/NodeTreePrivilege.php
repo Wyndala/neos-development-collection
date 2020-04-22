a3b8047f21ab2245e84391ee70ed6b88e9caede1
r.michael@queo-group.com <r.michael@queo-group.com>
2020-04-22 14:36:45 +0200

    FEATURE: NodeTreePrivilege always gives EditNodePrivilege, despite EditNodePrivilege is not GRANTED Removed Inheritance from NodePrivileges at all, used ReadNodePrivilege instead as blueprint

--- Neos.Neos/Classes/Security/Authorization/Privilege/NodeTreePrivilege.php
+++ Neos.Neos/Classes/Security/Authorization/Privilege/NodeTreePrivilege.php
@@ -11,11 +11,34 @@ namespace Neos\Neos\Security\Authorization\Privilege;
  * source code.
  */
 
-use Neos\ContentRepository\Security\Authorization\Privilege\Node\EditNodePrivilege;
+use Neos\ContentRepository\Security\Authorization\Privilege\Node\NodePrivilegeContext;
+use Neos\ContentRepository\Security\Authorization\Privilege\Node\NodePrivilegeSubject;
+use Neos\Eel\CompilingEvaluator;
+use Neos\Eel\Context;
+use Neos\Flow\Security\Authorization\Privilege\AbstractPrivilege;
+use Neos\Flow\Security\Authorization\Privilege\PrivilegeSubjectInterface;
+use Neos\Flow\Security\Exception\InvalidPrivilegeTypeException;
 
 /**
- * A privilege to show (document) nodes in the navigate component of the Neos backend. This also includes any manipulation of the affected nodes
+ * A privilege to show (document) nodes in the navigate component of the Neos backend.
  */
-class NodeTreePrivilege extends EditNodePrivilege
+class NodeTreePrivilege extends AbstractPrivilege
 {
+
+    /**
+     * @param PrivilegeSubjectInterface $subject
+     * @return bool
+     * @throws InvalidPrivilegeTypeException
+     * @throws \Neos\Flow\Security\Exception
+     */
+    public function matchesSubject(PrivilegeSubjectInterface $subject)
+    {
+        if (!$subject instanceof NodePrivilegeSubject) {
+            throw new InvalidPrivilegeTypeException(sprintf('Privileges of type "%s" only support subjects of type "%s", but we got a subject of type: "%s".', static::class, NodePrivilegeSubject::class, get_class($subject)), 1465979693);
+        }
+        $nodeContext = new NodePrivilegeContext($subject->getNode());
+        $eelContext = new Context($nodeContext);
+        $eelCompilingEvaluator = $this->objectManager->get(CompilingEvaluator::class);
+        return $eelCompilingEvaluator->evaluate($this->getParsedMatcher(), $eelContext);
+    }
 }
